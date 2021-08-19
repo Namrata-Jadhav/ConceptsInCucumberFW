21b5cf17f11f2cc24396ef313453364d3bc16369
Namrata-Jadhav <jadhavnamrata09@gmail.com>
2021-08-19 06:16:11 -0700

    BeforeStep nd AfterStep hooks are added with screenshot capturing

--- src/test/java/StepDefs/StepDefs.java
+++ src/test/java/StepDefs/StepDefs.java
@@ -1,11 +1,11 @@
 package StepDefs;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
+import io.cucumber.java.*;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import java.util.List;
@@ -14,20 +14,42 @@ import java.util.Map;
 public class StepDefs {
 
     WebDriver driver;
+    Scenario scenario;
 
+    @Before
+    public void setUp(Scenario scenario){
+        this.scenario=scenario;
+    }
+    @After
     public void CleanUp() {
         if (!(driver == null)) {
             driver.quit();
         }
     }
+
+    @BeforeStep
+    public void beforeEachStep(){
+       scenario.log("Executed before step");
+    }
+    @AfterStep
+    public void afterEachStep(){
+        if(!(driver==null)) {
+            TakesScreenshot scrnShot = (TakesScreenshot) driver;
+            byte[] data = scrnShot.getScreenshotAs(OutputType.BYTES);
+            scenario.attach(data, "image/png", "Failed step names:" + scenario.getName());
+            scenario.log("Executed after step");
+        }
+    }
+
     @Given("I want to do something")
     public void i_want_to_do_something(){
     }
 
-    @When("I have a argumentts to send as")
+    @When("I have a arguments to send as")
     public void i_have_a_argument_to_send_as(String arg) {
         System.out.println("Arguments to be passed as: "+arg);
     }
+
     @When("I have a list of students to send as")
     public void i_have_a_list_of_students_to_send_as(List<String> list) {
         // Write code here that turns the phrase above into concrete actions
@@ -39,6 +61,7 @@ public class StepDefs {
         // For other transformations you can register a DataTableType.
         System.out.println(list.toString());
     }
+
     @When("I have a list of students and their marks to send as")
     public void i_have_a_list_of_students_and_their_marks_to_send_as(Map<String, String> map ) {
         // Write code here that turns the phrase above into concrete actions
@@ -69,7 +92,6 @@ public class StepDefs {
     public void search_result_should_be_displayed_to(String string) {
         System.out.println("Product search success: "+string);
     }
-
 
 
 }
